a1100d4b5f8dabf677351cb1a577556cbdc17d18
mohammed aljafar <xqg1122@gmail.com>
2023-08-01 00:01:43 +0300

    Learn about inhertiance and interfaces.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,44 @@
-class Ride {
-	private static activeRides: number = 0;
+class Person {
+	constructor(public firstName: string, public lastName: string) {}
 
-	start() {
-		Ride.activeRides++;
+	get fullName(): string {
+		return `${this.firstName} ${this.lastName}`;
 	}
 
-	stop() {
-		Ride.activeRides--;
-	}
-
-	get activeRides(): number {
-		return Ride.activeRides;
+	walk(): void {
+		console.log(`${this.fullName} is walking`);
 	}
 }
+
+class Student extends Person {
+	constructor(public readonly studentID: number, firstName: string, lastName: string) {
+		super(firstName, lastName);
+	}
+}
+
+interface Address {
+	street: string;
+	city: string;
+	zipcode: string;
+}
+
+interface EmployeeInterface {
+	name: string;
+	salary: number;
+	address: Address;
+}
+
+class Employee extends Person implements EmployeeInterface {
+	constructor(
+		firstName: string,
+		lastName: string,
+		public salary: number,
+		public address: Address,
+	) {
+		super(firstName, lastName);
+	}
+	name: string = this.fullName;
+}
+
+const student = new Student(1, 'mohammed', 'aljafar');
+student.walk();
