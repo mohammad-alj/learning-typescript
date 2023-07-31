3528c48999dfd5cee5f670e6bf657a36d119475a
mohammed aljafar <xqg1122@gmail.com>
2023-07-31 21:17:03 +0300

    Learn about access modifiers and getters.:

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
 class BankAcount {
 	readonly id: number;
 	owner: string;
-	balance: number;
+	private _balance: number;
 
 	constructor(id: number, owner: string, balance: number) {
 		this.id = id;
 		this.owner = owner;
-		this.balance = balance;
+		this._balance = balance;
 	}
 
 	deposit(amount: number): void {
 		if (amount <= 0) throw new Error('Invalid amount');
-		this.balance += amount;
+		this._balance += amount;
+	}
+
+	get balance(): number {
+		return this._balance;
 	}
 }
 
 const myAcount = new BankAcount(0, 'mohammed', 100);
-console.log(myAcount.balance);
 console.log(myAcount instanceof BankAcount);
+console.log(myAcount.balance);
