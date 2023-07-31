9903e5b6500a6df998013b11f7972d626ea16478
mohammed aljafar <xqg1122@gmail.com>
2023-07-31 21:22:38 +0300

    Learn about getters and setters.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 class BankAcount {
 	constructor(public readonly id: number, public owner: string, private _balance: number) {}
 
-	deposit(amount: number): void {
-		if (amount <= 0) throw new Error('Invalid amount');
-		this._balance += amount;
-	}
-
 	get balance(): number {
 		return this._balance;
+	}
+
+	set balance(value: number) {
+		if (value <= 0) throw new Error('Balance must be greater than 0');
+		this._balance = value;
 	}
 }
 
