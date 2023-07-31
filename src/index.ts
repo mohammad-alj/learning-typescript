9830d35c044fbb27ec09e25ed0430b40d48ec53a
mohammed aljafar <xqg1122@gmail.com>
2023-07-31 21:10:01 +0300

    Learn about creating objects.

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
 class BankAcount {
-    id: number;
-    owner: string;
-    balance: number;
+	id: number;
+	owner: string;
+	balance: number;
 
-    constructor(id: number, owner: string, balance: number) {
-        this.id = id;
-        this.owner = owner;
-        this.balance = balance;
-    }
+	constructor(id: number, owner: string, balance: number) {
+		this.id = id;
+		this.owner = owner;
+		this.balance = balance;
+	}
 
-    deposit(amount: number): void {
-        if (amount <= 0)
-            throw new Error('Invalid amount')
-        this.balance += amount;
-    }
+	deposit(amount: number): void {
+		if (amount <= 0) throw new Error('Invalid amount');
+		this.balance += amount;
+	}
 }
+
+const myAcount = new BankAcount(0, 'mohammed', 100);
+console.log(myAcount.balance);
+console.log(myAcount instanceof BankAcount);
