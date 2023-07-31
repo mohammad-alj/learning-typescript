8b40a2f82aa3722e7b6d0ca0be375f26a862e012
mohammed aljafar <xqg1122@gmail.com>
2023-07-31 21:36:38 +0300

    Learn about index signatures.

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,5 @@
-class BankAcount {
-	constructor(public readonly id: number, public owner: string, private _balance: number) {}
-
-	deposit(amount: number): void {
-		if (amount <= 0) throw new Error('Invalid amount');
-		this._balance += amount;
-	}
-
-	get balance(): number {
-		return this._balance;
-	}
-
-	// set balance(value: number) {
-	// 	if (value <= 0) throw new Error('Balance must be greater than 0');
-	// 	this._balance = value;
-	// }
-}
-
-const myAcount = new BankAcount(0, 'mohammed', 100);
-console.log(myAcount instanceof BankAcount);
-console.log(myAcount.balance);
+const freq = new (class {
+	[key: string]: string;
+})();
+freq['something'] = 'that';
+console.log(freq['something']);
